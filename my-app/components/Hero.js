111780673ddf99477bb226d7501a1411d644e637
Nathan Hu <nathanhu0321@gmail.com>
2022-10-28 05:12:02 +0200

    added new styling to hero section

--- my-app/components/Hero.js
+++ my-app/components/Hero.js
@@ -1,53 +1,39 @@
 import React from 'react';
-// import ParticlesBackground from './Particles';
 
 import styles from './../styles/Hero.module.css'
 
-import Lottie from 'react-lottie';
-import animationData from './images/heroLottie.json'
-
 
 import Image from 'next/image'
-import heroImage from './images/heroImage.png'
 
 
 const Hero = () => {
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
-
   return (
   <div className={styles.hero}>
-    <div className={styles.introCard}>
+
+      <div className={styles.introCard}>
       <div>
         <h3 className={styles.schematicStyle}> &lt;html&gt; </h3>
-        
+
       </div>
       <div className={styles.title}>
-        <h1>A personal blogging platform for  </h1>
-        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all things &#123;computer science&#125;</h1>
+        <h1>A <span style={{fontFamily: 'mavis-bold', color:'#7EDDD3'}}>personal </span> <span style={{fontFamily: 'mavis-bold', color:'#F44C7E'}}> blogging</span> site for</h1>
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all things <span style={{ fontFamily: 'mavis-bold', color: '#F44C7E' }}>&#123;</span> <span style={{fontFamily: 'mavis-bold', color:'#7EDDD3'}}>computer </span><span style={{fontFamily: 'mavis-bold', color:'#F44C7E'}}>science</span> <span style={{ fontFamily: 'mavis-bold', color: '#7EDDD3' }}>&#125;</span>
+        </h1>
       </div>
 
-      <h3 className={styles.schematicStyle}> &lt;html&gt; </h3>
+      
 
-      <div className={styles.lottie}>
-        <Lottie 
-        options={defaultOptions}
-          height={500}
-          width={500}
-        />
-      </div>
+      <span style={{ color: 'red' }}></span>
+
+      <h3 className={styles.schematicStyle}> &lt;/html&gt; </h3>
+
+      <h1><br></br></h1>
+      <h1><br></br></h1>
 
       {/* <h3 className={styles.schematicStyle}> &lt;author&gt; Nathan Hu &lt;author&gt;</h3> */}
       <h3 className={styles.schematicStyle}> &lt;author&gt; </h3>
       <h3 className={styles.schematicStyle}> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nathan Hu</h3>
-      <h3 className={styles.schematicStyle}> &lt;author&gt; </h3>
+      <h3 className={styles.schematicStyle}> &lt;/author&gt; </h3>
     </div>
 
 

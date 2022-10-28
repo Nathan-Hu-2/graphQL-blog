6e7f8e0f2d210e7fae7286af2ead2b7b6b413ffa
Nathan Hu <nathanhu0321@gmail.com>
2022-10-28 17:21:25 +0200

    fixed responsive issues...

--- my-app/components/Hero.js
+++ my-app/components/Hero.js
@@ -13,12 +13,13 @@ const Hero = () => {
       <div className={styles.introCard}>
       <div>
         <h3 className={styles.schematicStyle}> &lt;html&gt; </h3>
-
       </div>
       <div className={styles.title}>
         <h1>A <span style={{color:'#7EDDD3'}}>personal </span> <span style={{ color:'#F44C7E'}}> blogging</span> </h1>
-        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;site for all things <span style={{  color: '#F44C7E' }}>&#123;</span> <span style={{ color:'#7EDDD3'}}>computer </span><span style={{color:'#F44C7E'}}>science</span> <span style={{  color: '#7EDDD3' }}>&#125;</span>
+        <h1>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;site for all things
         </h1>
+        <h1><span style={{  color: '#F44C7E' }}>&#123;</span> <span style={{ color:'#7EDDD3'}}>computer </span><span style={{color:'#F44C7E'}}>science</span> <span style={{  color: '#7EDDD3' }}>&#125;</span></h1>
       </div>
 
       
@@ -27,7 +28,6 @@ const Hero = () => {
 
       <h3 className={styles.schematicStyle}> &lt;/html&gt; </h3>
 
-      <h1><br></br></h1>
       <h1><br></br></h1>
 
       {/* <h3 className={styles.schematicStyle}> &lt;author&gt; Nathan Hu &lt;author&gt;</h3> */}
